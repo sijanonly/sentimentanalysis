f61a302e1b9ba115179113707709c2dfddc803b1
sijanonly <sijanonly@gmail.com>
2019-01-19 14:44:26 +0100

    add heroku procfile config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lstmsentiment.wsgi --log-file -
--- lstmsentiment/settings.py
+++ lstmsentiment/settings.py
@@ -130,4 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+
+# APPS_DIR = BASE_DIR.path('portfolio')
+# print('app', APPS_DIR)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
